cdc4556614ab536b535ff59bf9606dd32f567af8
terunuma-t <terunuma_t@uuum.jp>
2024-05-16 15:08:18 +0900

    issue7のいいね投稿一覧以外をやりました

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,9 @@
 class Post < ApplicationRecord
 
+    has_many :favorites, dependent: :destroy
     belongs_to :user
 
+    def favorited_by?(user)
+        favorites.exists?(user_id: user)
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :posts
+  has_many :favorites, dependent: :destroy
 
 end
